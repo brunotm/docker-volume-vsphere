f410735c514a2729d178e465a418489b40b2616a
Bruno Moura <brunotm@gmail.com>
2016-11-08 12:47:33 +0000

    vmdk_ops: Check attached status in removeVMDK before trying to remove the volume

--- esx_service/vmdk_ops.py
+++ esx_service/vmdk_ops.py
@@ -402,8 +402,15 @@ def vol_info(vol_meta, vol_size_info, datastore):
 # Return error, or None for OK
 def removeVMDK(vmdk_path):
     logging.info("*** removeVMDK: %s", vmdk_path)
-    cmd = "{0} {1}".format(VMDK_DELETE_CMD, vmdk_path)
 
+    # Check the current volume status
+    attached, uuid, attach_as = getStatusAttached(vmdk_path)
+    if attached:
+        logging.info("*** removeVMDK: %s is in use, VM uuid = %s", vmdk_path, uuid)
+        return err("Failed to remove volume {0}, is in use by VM uuid = {1}.".format(
+            vmdk_path, uuid))
+
+    cmd = "{0} {1}".format(VMDK_DELETE_CMD, vmdk_path)
     # Workaround timing/locking issues.
     retry_count = 0
     while True:
