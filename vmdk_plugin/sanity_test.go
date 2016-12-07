36b3b48b58ac725578c855b96ab2798f35c937f2
Bruno Moura <brunotm@gmail.com>
2016-12-07 15:04:34 +0000

    Move concurrent tests to its own test case
    Add getClients() for client init/connect
    Change parallel* constants to flags @msterin

--- vmdk_plugin/sanity_test.go
+++ vmdk_plugin/sanity_test.go
@@ -40,13 +40,19 @@ const (
 	// tests are often run under regular account and have no access to /var/log
 	defaultTestLogPath = "/tmp/test-docker-volume-vsphere.log"
 	// Number of volumes per client for parallel tests
-	parallelVolumes = 9
 )
 
 var (
 	// flag vars - see init() for help
 	removeContainers bool
+	parallelVolumes  int
+	parallelClones   int
 )
+
+type testClient struct {
+	endPoint string
+	client   *client.Client
+}
 
 // prepares the environment. Kind of "main"-ish code for tests.
 // Parses flags and inits logs and mount ref counters (the latter waits on Docker
@@ -59,6 +65,8 @@ func init() {
 
 	flag.BoolVar(&removeContainers, "rm", true, "rm container after run")
 	flag.StringVar(&driverName, "d", "vmdk", "Driver name. We refcount volumes on this driver")
+	flag.IntVar(&parallelVolumes, "parallel_volumes", 5, "Volumes per docker daemon for create/delete concurrent tests")
+	flag.IntVar(&parallelClones, "parallel_clones", 3, "Volumes per docker daemon for clone concurrent tests")
 	flag.Parse()
 	usingConfigFileDefaults := logInit(logLevel, logFile, configFile)
 
@@ -166,17 +174,10 @@ func volumeVmdkExists(t *testing.T, c *client.Client, vol string) *types.Volume 
 	return nil
 }
 
-// Sanity test for VMDK volumes
-// - check we can attach/detach correct volume (we use 'touch' and 'stat' to validate
-// - check volumes are correctly created and deleted.
-// - check we see it properly from another docker VM (-H2 flag)
-func TestSanity(t *testing.T) {
+// Initialize the clients and connect with the docker daemons
+func getClients(t *testing.T) []testClient {
 
-	fmt.Printf("Running tests on  %s (may take a while)...\n", TestInputParamsUtil.GetEndPoint1())
-	clients := []struct {
-		endPoint string
-		client   *client.Client
-	}{
+	clients := []testClient{
 		{TestInputParamsUtil.GetEndPoint1(), new(client.Client)},
 		{TestInputParamsUtil.GetEndPoint2(), new(client.Client)},
 	}
@@ -189,7 +190,18 @@ func TestSanity(t *testing.T) {
 		t.Logf("Successfully connected to %s", elem.endPoint)
 		clients[idx].client = c
 	}
+	return clients
+}
 
+// Sanity test for VMDK volumes
+// - check we can attach/detach correct volume (we use 'touch' and 'stat' to validate
+// - check volumes are correctly created and deleted.
+// - check we see it properly from another docker VM (-H2 flag)
+func TestSanity(t *testing.T) {
+
+	fmt.Printf("Running tests on  %s (may take a while)...\n", TestInputParamsUtil.GetEndPoint1())
+
+	clients := getClients(t)
 	c := clients[0].client // this is the endpoint we use as master
 	volumeName := TestInputParamsUtil.GetVolumeName()
 	t.Logf("Creating vol=%s on client %s.", volumeName, clients[0].endPoint)
@@ -229,10 +241,20 @@ func TestSanity(t *testing.T) {
 				volumeName, elem.endPoint)
 		}
 	}
+}
 
-	fmt.Printf("Running parallel tests on %s and %s (may take a while)...\n", TestInputParamsUtil.GetEndPoint1(), TestInputParamsUtil.GetEndPoint2())
-	// Create a short buffered channel to introduce random pauses
+// Test concurrent volume operations
+// - concurrent create/delete btween different docker hosts
+// - concurrent clone/delete btween different docker hosts
+// - concurrent create/delete on the same docker host
+func TestConcurrency(t *testing.T) {
+
+	volumeName := "volTestP"
+	clients := getClients(t)
+
+	// Buffered channel to read results from
 	results := make(chan error, parallelVolumes)
+
 	createRequest := types.VolumeCreateRequest{
 		Name:   volumeName,
 		Driver: driverName,
@@ -240,11 +262,14 @@ func TestSanity(t *testing.T) {
 			"size": "1gb",
 		},
 	}
-	// Create/delete routine
+
+	fmt.Printf("Running create/delete parallel tests on %s and %s (may take a while)...\n",
+		clients[0].endPoint, clients[1].endPoint)
+	// Create/delete goroutine
 	for idx, elem := range clients {
 		go func(idx int, c *client.Client) {
 			for i := 0; i < parallelVolumes; i++ {
-				volName := "volTestP" + strconv.Itoa(idx) + strconv.Itoa(i)
+				volName := volumeName + strconv.Itoa(idx) + strconv.Itoa(i)
 				createRequest.Name = volName
 				_, err := c.VolumeCreate(context.Background(), createRequest)
 				results <- err
